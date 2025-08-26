c00e31b8765dafb44b563d2792dcb7ec721df4f7
Sanderson Rocha <sanduba753@gmail.com>
2025-08-26 15:08:09 -0300

    Folder config screen

--- src/component/folder-config.ts
+++ src/component/folder-config.ts
@@ -1,20 +1,19 @@
-import {LitElement, css, html} from 'lit';
+import { LitElement, css, html } from 'lit';
+import { InputMetadata } from '../common/input-metadata';
 export class FolderConfig extends LitElement {
   static readonly styles = css``;
 
-  private path: string = null
-  private _handlePath = (event:InputEvent) => this.path = (event.target as HTMLInputElement).value
+  pathMeta = new InputMetadata(new Date())
 
   render() {
     return html`
         <h3>Fonder config</h3>
         <p>Please inform here the folder with your songs</p>
-        <input type="text" .value="${this.path}" @input="${this._handlePath}"/>
+        <input type="date" .value="${this.pathMeta.val()}" @input="${this.pathMeta.val}"/>
         <button @click="${this.submit}">Submit</button>
     `
   }
-  submit(){
-    console.log(this.path);
-    
+  submit() {
+    console.log(this.pathMeta.val());
   }
 }
