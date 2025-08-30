eb9db0bed2dd010ac5579c908212b3c7cd4fa179
Sanderson Rocha <sanduba753@gmail.com>
2025-08-30 08:12:44 -0300

    Creation of redirection route at the '/' dir

--- src/config/route-list.ts
+++ src/config/route-list.ts
@@ -1,6 +1,15 @@
 import { Route } from '../io/type/route'
+import { globalState } from '../common/global-state'
 
 export const routeList: Route[] = [
     { path: '/folder-config', component: 'folder-config' },
-    { path: '/', action: () => { window.location.href = '/folder-config' } },
+    {
+        path: '/', action: () => {
+            if (!globalState.workDir) {
+                window.location.href = '/folder-config'
+                return
+            }
+            window.location.href = '/music'
+        }
+    },
 ]
